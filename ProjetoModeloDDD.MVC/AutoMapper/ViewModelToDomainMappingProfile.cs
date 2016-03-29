65daafbcc509c804acaddf03716ae31f3420d92f
Daniel Carneiro Miranda <daniel.c.miranda@terra.com.br>
2016-03-29 03:54:51 -0300

    AutoMapper

---
+++ ProjetoModeloDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel>();
+            Mapper.CreateMap<Produto, ProdutoViewModel>();
+        }
+    }
+}
