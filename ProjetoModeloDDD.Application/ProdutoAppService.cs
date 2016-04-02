8fe985c944cd32b6ee4a798c2170694aff851193
Daniel Carneiro Miranda <daniel.c.miranda@terra.com.br>
2016-04-02 09:29:01 -0300

    camada aplicacao

---
+++ ProjetoModeloDDD.Application/ProdutoAppService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+
+namespace ProjetoModeloDDD.Application
+{
+    public class ProdutoAppService : AppServiceBase<Produto>, IProdutoService
+    {
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoAppService(IProdutoService produtoService) 
+            : base(produtoService)
+        {
+            _produtoService = produtoService;
+        }
+
+        public IEnumerable<Produto> BuscarPorNome(string nome)
+        {
+            return _produtoService.BuscarPorNome(nome);
+        }
+    }
+}
